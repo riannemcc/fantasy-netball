13b755f382f14b5c6df5dca7053a870bed0d4d47
Rianne McCartney <rianne.mccartney@gousto.co.uk>
2019-11-08 11:05:20 +0000

    First commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+
+function Home() {
+    return (
+        <>
+            <h1>Fantasy Netball</h1>
+            <h2> Vitality Netball Superleague</h2>
+            <button>Sign up</button>
+            <button>Log in</button>
+        </>
+    );
+}
+
+export default Home;
