de65c32caee310773c0e7a050dfd642c3b23c298
Rianne <rianne.mccartney@gousto.co.uk>
2020-06-21 16:34:00 +0100

    Updating leaderboard to show team name

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,43 +1,61 @@
-import React from 'react';
-import { LeaderboardTable } from "./Tables/LeaderboardTable";
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import './Leaderboard.css'
 import {
-    BrowserRouter as Router,
     Link
   } from "react-router-dom";
+  import { withFirebase } from './Firebase'
 
 
-function Leaderboard() {
-    const hasTeam = true
+function Leaderboard({firebase}) {
+    const hasTeam = true 
+    // need to update thosi to check whether user has a team 
+    const [currentUser, setCurrentUser] = useState(null)
+    
+    useEffect(() => {
+        const fetchUser = (uid) => {
+            firebase.getUserData(uid)
+                .then((userSnapshot) => {
+                    setCurrentUser(userSnapshot.data())
+                })
+                .catch(console.log)
+        }
+
+        if (!currentUser && firebase.currentUserId) {
+            fetchUser(firebase.currentUserId)
+        }
+    }, [firebase.currentUserId, currentUser, firebase])
+
     return (
         <div>
           {hasTeam ? (
               <>
-                <h3>Your Team -</h3>
+                <h3>Your Team - {currentUser ? currentUser.teamName : ""} </h3>
                 <h3> Leaderboard</h3>
                 <table>
-                    <tr>
-                        <th>Player</th>
-                        <th>Team Name</th>
-                        <th>Round Two</th>
-                        <th>Round Three</th>
-                        <th>Round Four</th>
-                        <th>Round Five</th>
-                        <th>Round Six</th>
-                        <th>Total points</th>
+                    <tbody>
+                        <tr>
+                            <th>Player</th>
+                            <th>Team Name</th>
+                            <th>Round Two</th>
+                            <th>Round Three</th>
+                            <th>Round Four</th>
+                            <th>Round Five</th>
+                            <th>Round Six</th>
+                            <th>Total points</th>
 
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
                 </table>
             </>
             ) : (
@@ -50,4 +68,4 @@ function Leaderboard() {
     );
 }
 
-export default Leaderboard;
+export default withFirebase(Leaderboard);
