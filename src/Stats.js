3626189c68e3ba0742158d711d7e06d0b6d42a8f
Rianne <rianne.mccartney@gousto.co.uk>
2020-06-21 16:34:14 +0100

    Updating stats table

--- src/Stats.js
+++ src/Stats.js
@@ -1,70 +1,17 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import axios from 'axios'
-import { withFirebase } from './Firebase'
+import React from 'react';
 import { StatsTable } from "./Tables/StatsTable";
 import './Home.css';
 import './Stats.css'
 
 
 function Stats() {
-    // const [data, setData] = useState([]);
 
-    // useEffect(() => {
-    //     (async () => {
-    //       const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
-    //       setData(result.data);
-    //     })();
-    //   }, []);
-
-    // const columns = useMemo(
-    //     () => [
-    //       {
-    //         Header: "Player",
-    //         // First group columns
-    //         columns: [
-    //           {
-    //             Header: "Player",
-    //             accessor: "show.name"
-    //           },
-    //           {
-    //             Header: "Team",
-    //             accessor: "show.type"
-    //           }
-    //         ]
-    //       },
-    //       {
-    //         // Second group - Details
-    //         Header: "Details",
-    //         // Second group columns
-    //         columns: [
-    //           {
-    //             Header: "Language",
-    //             accessor: "show.language"
-    //           },
-    //           {
-    //             Header: "Genre(s)",
-    //             accessor: "show.genres"
-    //           },
-    //           {
-    //             Header: "Runtime",
-    //             accessor: "show.runtime"
-    //           },
-    //           {
-    //             Header: "Status",
-    //             accessor: "show.status"
-    //           }
-    //         ]
-    //       }
-    //     ],
-    //     []
-    //   );
 
     return (
         <div className="container">
             <h2>
                 Stats
             </h2>
-            {/* <StatsTable columns={columns} data={data} /> */}
             <div className="teamButtons">
                 <button className="teamButton">Celtic Dragons</button>
                 <button className="teamButton">Saracens Mavericks</button>
@@ -77,39 +24,7 @@ function Stats() {
                 <button className="teamButton">Wasps Netball</button>
                 <button className="teamButton">Surrey Storm</button>
             </div>
-            <table>
-                <tr>
-                    <th>Player</th>
-                    <th>Round One</th>
-                    <th>Round Two</th>
-                    <th>Round Three</th>
-                    <th>Round Four</th>
-                    <th>Round Five</th>
-                    <th>Round Six</th>
-                    <th>Total points</th>
-
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                </table>
+           <StatsTable />
         </div>
     );
 }
--- src/Tables/StatsTable.js
+++ src/Tables/StatsTable.js
@@ -1,31 +1,105 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withFirebase } from '../Firebase';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-const StatsTable = () => {
-      return (
-        <table>
-          <thead>
-            {/* {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                  <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-                ))}
-              </tr>
-            ))} */}
-          </thead>
-          <tbody>
-            {/* {rows.map((row, i) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map(cell => {
-                    return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-                  })}
-                </tr>
-              );
-            })} */}
-          </tbody>
-        </table>
-      );
-}
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
 
-export { StatsTable }
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+
+
+export const StatsTable = withFirebase(({ firebase }) => {
+  const [playersLoaded, setPlayersLoaded] = useState(false)
+  const [players, setPlayers] = useState([])
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 700,
+    },
+  });
+
+  useEffect(() => {
+    const fetchPlayers = () => {
+      firebase.players()
+        .then((playersSnapshot) => {
+          console.log("Pla: ", playersSnapshot)
+          setPlayers(playersSnapshot.docs)
+          setPlayersLoaded(true)
+        })
+        .catch(console.log)
+    }
+
+    if (!playersLoaded) {
+      fetchPlayers()
+    }
+  }, [firebase, playersLoaded])
+
+  const classes = useStyles()
+  return (
+    <TableContainer component={Paper} >
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Player</StyledTableCell>
+            <StyledTableCell align="right">Round One</StyledTableCell>
+            <StyledTableCell align="right">Round Two</StyledTableCell>
+            <StyledTableCell align="right">Round Three</StyledTableCell>
+            <StyledTableCell align="right">Round Four</StyledTableCell>
+            <StyledTableCell align="right">Round Five</StyledTableCell>
+            <StyledTableCell align="right">Round Six</StyledTableCell>
+            <StyledTableCell align="right">Round Seven</StyledTableCell>
+            <StyledTableCell align="right">Round Eight</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <StyledTableRow>
+            <StyledTableCell component="th" scope="row">Tst</StyledTableCell>
+            <StyledTableCell align="right">10</StyledTableCell>
+            <StyledTableCell align="right">25</StyledTableCell>
+            <StyledTableCell align="right">25</StyledTableCell>
+            <StyledTableCell align="right">25</StyledTableCell>
+            <StyledTableCell align="right">25</StyledTableCell>
+            <StyledTableCell align="right">25</StyledTableCell>
+            <StyledTableCell align="right">25</StyledTableCell>
+            <StyledTableCell align="right">25</StyledTableCell>
+          </StyledTableRow>
+          {players.map((playerDoc) => {
+            return (
+              <StyledTableRow>
+                <StyledTableCell component="th" scope="row">{playerDoc.get('name')}</StyledTableCell>
+                <StyledTableCell align="right">10</StyledTableCell>
+                <StyledTableCell align="right">25</StyledTableCell>
+                <StyledTableCell align="right">25</StyledTableCell>
+                <StyledTableCell align="right">25</StyledTableCell>
+                <StyledTableCell align="right">25</StyledTableCell>
+                <StyledTableCell align="right">25</StyledTableCell>
+                <StyledTableCell align="right">25</StyledTableCell>
+                <StyledTableCell align="right">25</StyledTableCell>
+              </StyledTableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+})
